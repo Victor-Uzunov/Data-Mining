5e6390201ce47904d6d297fd59882ee4f1bb3708
VicZuno <uzunovvictor@gmail.com>
2025-11-29 12:44:55 +0200

    Ignore type

--- algorithms/dfs-and-bfs/graph_traversal.py
+++ algorithms/dfs-and-bfs/graph_traversal.py
@@ -60,7 +60,7 @@ def breadth_first_search(graph: Graph, start_node: str):
 
     return result
 
-def depth_limited_search(graph: dict, start_node: str, goal_node: str, limit: int) -> list or None:
+def depth_limited_search(graph: dict, start_node: str, goal_node: str, limit: int) -> list or None: # type: ignore
     stack = [(start_node, [start_node], 0)]
     while stack:
         cur_node, path, depth = stack.pop()
@@ -73,7 +73,7 @@ def depth_limited_search(graph: dict, start_node: str, goal_node: str, limit: in
 
     return None
 
-def iterative_deepening_search(graph: dict, start_node: str, goal_node: str, max_depth: int) -> list or None:
+def iterative_deepening_search(graph: dict, start_node: str, goal_node: str, max_depth: int) -> list or None: # type: ignore
     for limit in range(max_depth + 1):
         result = depth_limited_search(graph, start_node, goal_node, limit)
         if result is not None:
